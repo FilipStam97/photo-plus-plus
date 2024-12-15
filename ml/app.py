f7a6e82b7cb6a7b9944f68059285e5a293c58a70
Anastasija <anastasija.savov2000@gmail.com>
2024-12-15 21:42:26 +0100

    add comments

--- ml/app.py
+++ ml/app.py
@@ -12,7 +12,7 @@ def get_face_embeddings(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
     face_locations = face_recognition.face_locations(image)
-    face_encodings = face_recognition.face_encodings(image, face_locations)
+    face_encodings = face_recognition.face_encodings(image, face_locations) 
     
     return face_encodings
 
@@ -22,18 +22,22 @@ def cluster_faces():
     if 'image_file' not in request.files:
         return jsonify({'error': 'No image part'}), 400
     
+    #convert file to image file 
     image_file = request.files['image_file']
     pillow_image = Image.open(image_file)
+    #convert image to 2d array
     image = np.asarray(pillow_image)
 
     face_embeddings = get_face_embeddings(image)
 
+    #create clusters of faces
     dbscan = DBSCAN(eps=0.6, min_samples=1) 
     dbscan.fit(face_embeddings)
     
     labels = dbscan.labels_
     
     clusters = {}
+    #create JSON response with clusters and face indices 
     for idx, label in enumerate(labels):
         label = int(label)
         if label not in clusters:
